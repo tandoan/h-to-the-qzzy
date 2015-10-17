95e6d1c45fb4b5b64114c216c79e5b88b602c1cf
Tan Doan <td@tandoan.com>
2015-10-16 22:35:13 -0700

    Polish: make getRandomLyric not return the current lyric
    Also move Session.get/set("currentLyric") into their own functions

--- h-to-the-qzzy.js
+++ h-to-the-qzzy.js
@@ -1,22 +1,45 @@
 Lyrics = new Mongo.Collection("lyrics");
 
-function getRandomLyric(){
-  var arr = Lyrics.find().fetch();
-  var randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
+function getCurrentLyric(){
+  return Session.get("currentLyric");
 }
+
+function setCurrentLyric(lyric){
+  return Session.set("currentLyric", lyric);
+}
+function getRandomLyric(){
+  var ret,
+  current = Session.get("currentLyric"),
+  shuffledLyrics = _.shuffle(Lyrics.find().fetch());
+
+debugger;
+  if(current){
+    do {
+      ret = shuffledLyrics.shift();
+    } while(ret._id.toString() === current._id.toString());
+    console.log(ret,current);
+  } else {
+      ret = shuffledLyrics.shift();
+  }
+
+  return ret;
+}
+
 function getAndSetRandomLyric(){
-  var lyric = getRandomLyric();
-  Session.set("currentLyric", lyric);
+  setCurrentLyric(getRandomLyric());
 }
 
 function uncheckAllQuestions(){
-      console.log('unchecking');
-      // use attr selector to to select only the checked one and uncheck it
-      $(".question input").map(function(i,elem){ 
-        $(elem).attr('checked',false);
-      });
+  $(".question input:checked").attr('checked',false);
 }
+
+function resetAndGetNewQuestion(){
+  Session.set("answer", '');
+  Session.set("correctness", '');
+  uncheckAllQuestions();
+  getAndSetRandomLyric();
+}
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -25,10 +48,10 @@ if (Meteor.isClient) {
   
   Template.question.helpers({
    lyric: function(){
-      var l = Session.get("currentLyric" ) 
+      var l = getCurrentLyric();
       if(!l){
         getAndSetRandomLyric();
-        l = Session.get("currentLyric" ) 
+        l = getCurrentLyric();
       }
       return l;
    }, 
@@ -53,7 +76,7 @@ if (Meteor.isClient) {
 
     "change .question": function (event){
       console.log('changed!')
-        var current = Session.get("currentLyric");
+        var current = getCurrentLyric();
         Session.answer = current.artist;
 
         if( event.target.value == current._id.toString()){
@@ -67,12 +90,7 @@ if (Meteor.isClient) {
         Session.set("totalAttempts", Session.get("totalAttempts")+1);
 
         // reset the board
-        setTimeout(function(){
-          Session.set("answer", '');
-          Session.set("correctness", '');
-          uncheckAllQuestions();
-          getAndSetRandomLyric();
-        }, 1000);
+        setTimeout(resetAndGetNewQuestion, 1000);
 
     }
   })
