ab4cd00dbfa9eb78b0eb6aac1777a6ddbc348767
Tan Doan <td@tandoan.com>
2015-10-19 09:36:48 -0700

    Make server add lyrics from fixtures if DB is initially empty

--- questions/server/index.js
+++ questions/server/index.js
@@ -1,7 +1,17 @@
 Meteor.startup(function () {
 
-            Lyrics = new Mongo.Collection("lyrics");
-    //if (Lyrics.find().count() === 0) {
-     // Lyrics.insert({	artist: "Kanye West",lyric: "Mayonaise colored Benz, I push Miricle Whips"});
-    //}
-  });
+	Lyrics = new Mongo.Collection("lyrics");
+	if (Lyrics.find().count() === 0) {
+		console.log('initializing db');
+
+		var fs = Npm.require('fs');
+		//TODO: find a saner path to read the fixture
+		//current path is coming from .meteor/local/build/..etc..
+		var contents = fs.readFileSync('../../../../../private/fixtures', {encoding: 'utf8'});
+
+		var json = JSON.parse(contents);
+		_.each(json,function(lyric){
+			Lyrics.insert(lyric);
+		});
+	}
+});
